bfaa0aa74e3da5ccd5ceebcf569b5d44469d4204
Vin Busquet <computationalcore@gmail.com>
2019-05-20 20:56:38 -0300

    feat: Add replications and movement replication to server instance (authority)

--- Source/PuzzlePlatforms/MovingPlatform.h
+++ Source/PuzzlePlatforms/MovingPlatform.h
@@ -18,6 +18,8 @@ class PUZZLEPLATFORMS_API AMovingPlatform : public AStaticMeshActor
 public:
 	AMovingPlatform();
 
+	virtual void BeginPlay() override;
+
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere)
